2441bed3872150b160998fb494e2040f88cf224a
Likhith <113507338+likhith2616@users.noreply.github.com>
2022-09-14 14:35:02 +0530

    Update streamlit_app.py
    Added to pandas.

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,5 +14,6 @@ streamlit.text('4. Avacado')
 streamlit.text('-------------------------------------------------------------')
 
 streamlit.header('Build Your Own Fruit Smoothie')
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
