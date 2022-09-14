9da116b68eba1360ce541367e72447547654ec4e
Likhith <113507338+likhith2616@users.noreply.github.com>
2022-09-14 14:20:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ streamlit.header('Breakfast Menu')
 streamlit.text('1. Oats & Corns')
 streamlit.text('2. Spinach')
 streamlit.text('3. cakes')
+-------------------------------------------------------------
