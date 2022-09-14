c2c3ee38c2248ccc70f4f2a93ae8df601fde9057
Likhith <113507338+likhith2616@users.noreply.github.com>
2022-09-14 14:33:45 +0530

    Update streamlit_app.py
    Added data from s3 bucket.

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,5 @@ streamlit.text('4. Avacado')
 streamlit.text('-------------------------------------------------------------')
 
 streamlit.header('Build Your Own Fruit Smoothie')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
